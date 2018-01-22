1ac6ba5952b2708687875faf868e69809adc733d
Chris Williams <christopher.williams@physics.ox.ac.uk>
2018-01-22 12:53:18 +0000

    new: enable astroaccelerate option

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,11 +4,16 @@ include(cmake/googletest.cmake)
 include(cmake/boost.cmake)
 find_package(Panda REQUIRED)
 find_package(Cheetah REQUIRED)
+find_package(AstroAccelerate)
 include(cmake/hiredis.cmake)
 
 include_directories(SYSTEM ${Boost_INCLUDE_DIR} ${CHEETAH_INCLUDE_DIR} ${PANDA_INCLUDE_DIR})
+if(ASTROACCELERATE_FOUND)
+    include_directories(SYSTEM ${ASTROACCELERATE_INCLUDE_DIR})
+endif(ASTROACCELERATE_FOUND)
 set(DEPENDENCY_LIBRARIES
     ${CHEETAH_LIBRARIES}
+    ${ASTROACCELERATE_LIBRARIES}
     ${PANDA_LIBRARIES}
     ${Boost_LIBRARIES}
     ${HIREDIS_LIBRARIES}
